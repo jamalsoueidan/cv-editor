2ab400b52f2da577ec4720d92817a1dbd63cb34a
Jamal Soueidan <jamal@soueidan.com>
2024-10-31 09:45:49 +0100

    better index page

--- app/components/HeaderSimple.tsx
+++ app/components/HeaderSimple.tsx
@@ -1,47 +1,116 @@
-import { Burger, Container, Group, Title } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
+import { useAuthActions } from "@convex-dev/auth/react";
+import {
+  Avatar,
+  Container,
+  Group,
+  Menu,
+  rem,
+  Text,
+  Title,
+  UnstyledButton,
+  useMantineTheme,
+} from "@mantine/core";
+import cx from "clsx";
+import { Authenticated } from "convex/react";
 import { useState } from "react";
-import { FaBeer } from "react-icons/fa";
+import { FaArrowDown, FaBeer, FaHeart, FaStar } from "react-icons/fa";
+import { IoIosLogOut } from "react-icons/io";
+import { VscAccount } from "react-icons/vsc";
 import classes from "./HeaderSimple.module.css";
 
-const links = [
-  { link: "/about", label: "Features" },
-  { link: "/pricing", label: "Pricing" },
-  { link: "/learn", label: "Learn" },
-  { link: "/community", label: "Community" },
-];
+const user = {
+  name: "Jane Spoonfighter",
+  email: "janspoon@fighter.dev",
+  image:
+    "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-5.png",
+};
 
 export function HeaderSimple() {
-  const [opened, { toggle }] = useDisclosure(false);
-  const [active, setActive] = useState(links[0].link);
+  const theme = useMantineTheme();
 
-  const items = links.map((link) => (
-    <a
-      key={link.label}
-      href={link.link}
-      className={classes.link}
-      data-active={active === link.link || undefined}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(link.link);
-      }}
-    >
-      {link.label}
-    </a>
-  ));
+  const [userMenuOpened, setUserMenuOpened] = useState(false);
+  const { signOut } = useAuthActions();
 
   return (
     <header className={classes.header}>
-      <Container size="md" className={classes.inner}>
-        <Group>
-          <FaBeer /> <Title order={3}>CV Online</Title>
-        </Group>
+      <Container className={classes.mainSection}>
+        <Group justify="space-between">
+          <Group>
+            <FaBeer />
+            <Title order={3}>CV.dk</Title>
+          </Group>
 
-        <Group gap={5} visibleFrom="xs">
-          {items}
-        </Group>
+          <Authenticated>
+            <Menu
+              width={260}
+              position="bottom-end"
+              transitionProps={{ transition: "pop-top-right" }}
+              onClose={() => setUserMenuOpened(false)}
+              onOpen={() => setUserMenuOpened(true)}
+              withinPortal
+            >
+              <Menu.Target>
+                <UnstyledButton
+                  className={cx(classes.user, {
+                    [classes.userActive]: userMenuOpened,
+                  })}
+                >
+                  <Group gap={7}>
+                    <Avatar
+                      src={user.image}
+                      alt={user.name}
+                      radius="xl"
+                      size={20}
+                    />
+                    <Text fw={500} size="sm" lh={1} mr={3}>
+                      {user.name}
+                    </Text>
+                    <FaArrowDown style={{ width: rem(12), height: rem(12) }} />
+                  </Group>
+                </UnstyledButton>
+              </Menu.Target>
+              <Menu.Dropdown>
+                <Menu.Item
+                  leftSection={
+                    <FaHeart
+                      style={{ width: rem(16), height: rem(16) }}
+                      color={theme.colors.red[6]}
+                    />
+                  }
+                >
+                  Cver
+                </Menu.Item>
+                <Menu.Item
+                  leftSection={
+                    <FaStar
+                      style={{ width: rem(16), height: rem(16) }}
+                      color={theme.colors.yellow[6]}
+                    />
+                  }
+                >
+                  Ansøgninger
+                </Menu.Item>
 
-        <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
+                <Menu.Label>Settings</Menu.Label>
+                <Menu.Item
+                  leftSection={
+                    <VscAccount style={{ width: rem(16), height: rem(16) }} />
+                  }
+                >
+                  Account settings
+                </Menu.Item>
+                <Menu.Item
+                  leftSection={
+                    <IoIosLogOut style={{ width: rem(16), height: rem(16) }} />
+                  }
+                  onClick={signOut}
+                >
+                  Logout
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          </Authenticated>
+        </Group>
       </Container>
     </header>
   );
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,29 +1,74 @@
 import { useAuthActions } from "@convex-dev/auth/react";
-import { Button, Container } from "@mantine/core";
-import { Link } from "@remix-run/react";
+import {
+  Button,
+  Card,
+  Container,
+  Flex,
+  Group,
+  Tabs,
+  Text,
+  Title,
+} from "@mantine/core";
 import { Authenticated, Unauthenticated } from "convex/react";
+import { GiPapers } from "react-icons/gi";
+import { GoPlus } from "react-icons/go";
 import { HeaderSimple } from "~/components/HeaderSimple";
 import { ResumesList } from "~/components/ResumesList";
-import { SignOut } from "~/components/Signout";
-
 export default function Index() {
   const { signIn } = useAuthActions();
-  return (
-    <Container>
-      <HeaderSimple />
-      <Authenticated>
-        <SignOut />
-        <ResumesList />
-        <br />
-        <Link to="/reader">Reader</Link>
-        <Link to="/test">Test</Link>
-      </Authenticated>
 
-      <Unauthenticated>
-        <Button onClick={() => void signIn("linkedin")}>
-          Sign in with Linkedin
-        </Button>
-      </Unauthenticated>
-    </Container>
+  return (
+    <>
+      <HeaderSimple />
+      <Container>
+        <Authenticated>
+          <Card bg="blue.1" radius="lg" my="xl">
+            <Group>
+              <GiPapers size="5rem" />
+              <Flex direction="column" gap="xs">
+                <Title order={4} fw="400">
+                  Opret dit cv
+                </Title>
+                <Text>
+                  Vi hjælper dig med at organisere din cv&apos;er online
+                </Text>
+              </Flex>
+            </Group>
+          </Card>
+
+          <Title order={3} mb="xs">
+            Dokumenter
+          </Title>
+
+          <Tabs defaultValue="resumes">
+            <Tabs.List>
+              <Tabs.Tab value="resumes">CV&apos;er</Tabs.Tab>
+              <Tabs.Tab value="coverletters">Ansøgninger</Tabs.Tab>
+              <Tabs.Tab value="account" ml="auto">
+                <Button size="compact-sm" leftSection={<GoPlus />} fw="500">
+                  Opret ny(t)
+                </Button>
+              </Tabs.Tab>
+            </Tabs.List>
+
+            <Tabs.Panel value="resumes" mt="xl">
+              <ResumesList />
+            </Tabs.Panel>
+
+            <Tabs.Panel value="coverletters" mt="xl">
+              ...
+            </Tabs.Panel>
+          </Tabs>
+
+          <br />
+        </Authenticated>
+
+        <Unauthenticated>
+          <Button onClick={() => void signIn("linkedin")}>
+            Sign in with Linkedin
+          </Button>
+        </Unauthenticated>
+      </Container>
+    </>
   );
 }
